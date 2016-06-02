b3c26cc7857c1dce338742802dbf48bbcbf75b7f
natsu <natsugtr@gmail.com>
2016-06-01 20:35:16 -0400

    TEST

--- js/dolphin.js
+++ js/dolphin.js
@@ -1,6 +1,18 @@
 $(document).ready( function(){
 
-	console.log("It begans");
+	//console.log("It begans");
+	
+	$("#submit").click( function(){
+
+		alert("submit is clicked")
+
+		
+
+
+
+
+
+		});
 
 
 });
